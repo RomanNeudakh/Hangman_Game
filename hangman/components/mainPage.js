7dd6c609e8a85ddfc5a07e5e7238814d81c6a2e3
Roman <neudakhroman@gmail.com>
2024-05-16 15:37:53 +0200

    feat: change dir srtucture, vite, lite-server, fix router

---
+++ hangman/components/mainPage.js
@@ -0,0 +1,5 @@
+export function renderMainPage() {
+    let main = document.querySelector('.main')
+    main.innerHTML = 'Main page'
+    return true
+}
---
+++ hangman/components/recordsPage.js
@@ -0,0 +1,5 @@
+export function renderRecordsPage() {
+    let main = document.querySelector('.main')
+    main.innerHTML = 'Records page'
+    return true
+}
